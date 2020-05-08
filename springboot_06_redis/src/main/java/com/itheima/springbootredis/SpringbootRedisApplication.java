9c1cc586c7709be013ed64d0535d62e394e41249
KyleSun <swy0907163@163.com>
2020-05-09 02:21:51 +0800

    springboot整合redis

---
+++ springboot_06_redis/src/main/java/com/itheima/springbootredis/SpringbootRedisApplication.java
@@ -0,0 +1,17 @@
+package com.itheima.springbootredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Program: SpringBoot
+ * @ClassName: SpringbootRedisApplication
+ * @Description:
+ * @Author: KyleSun
+ **/
+@SpringBootApplication
+public class SpringbootRedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRedisApplication.class, args);
+    }
+}
---
+++ springboot_06_redis/src/test/java/com/itheima/springbootredis/SpringbootRedisApplicationTest.java
@@ -0,0 +1,33 @@
+package com.itheima.springbootredis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Program: SpringBoot
+ * @ClassName: SpringbootRedisApplicationTest
+ * @Description:
+ * @Author: KyleSun
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootRedisApplicationTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void setTest() {
+        redisTemplate.boundValueOps("name").set("KyleSun");
+    }
+
+    @Test
+    public void getTest() {
+        Object name = redisTemplate.boundValueOps("name").get();
+        System.out.println(name);
+    }
+}
